8bebaa67a554d790829d479c3f1c67b71c20cc2d
NikuBaguette <81246303+NikuBaguette@users.noreply.github.com>
2022-10-12 09:47:34 +0200

    Add files via upload

--- Players.py
+++ Players.py
@@ -1,17 +1,55 @@
+import pygame
 class New_Players:
     count = 0
-    def __init__(self,name="NEW_PLAYER",life=1000,height=100,width=60):
-        self.name = name
-        self.hp = life
-        self.height = height
-        self.width = width
-        self.Stunned = False
-    
-    def GetStunned(self,time=1):
-        self.Stunned=True
-        #wait for time sec
-        self.Stunned=False
-        return None
+    def __init__(self,width=150,height=200,x=0,y=0):
+        self.x = x
+        self.y = y
+        self.vel = 0
+        self.jumping = False
+        self.HurtBox = pygame.Rect(x,y,width,height)
         
-    def IsStunned(self):
-        return self.Stunned
+    def Draw(self,surface):
+        pygame.draw.rect(surface,(255,0,0),self.HurtBox)
+    
+    def Attack(self,surface):
+        Hitbox = pygame.Rect(self.HurtBox.centerx,self.HurtBox.y,2*self.HurtBox.width,self.HurtBox.height)
+        pygame.draw.rect(surface,(0,255,0),Hitbox)
+
+    def Movement(self,surface,height,width):
+        sp = 10
+        grv = 4
+        dx = 0
+        dy = 0
+        key = pygame.key.get_pressed()
+
+        if key[pygame.K_q]:
+            dx=-sp
+        
+        if key[pygame.K_d]:
+            dx=sp
+
+        if self.jumping == False and key[pygame.K_z]:
+            self.jumping = True
+            self.vel = -50
+
+        self.vel += grv
+        dy += self.vel 
+
+        if key[pygame.K_r]:
+            self.Attack(surface)
+        
+        if self.HurtBox.left + dx < 0:
+            dx = -self.HurtBox.left
+        
+        if self.HurtBox.right + dx > width:
+            dx = width - self.HurtBox.right
+        
+        if self.HurtBox.bottom + dy > height - 100:
+            self.vel = 0
+            dy = height - 100 - self.HurtBox.bottom
+            self.jumping = False
+        
+        self.HurtBox.x += dx
+        self.HurtBox.y += dy
+    
+    
--- SuperGame.py
+++ SuperGame.py
@@ -1,9 +1,9 @@
 import pygame as pg
-import Players as plr
+import Players
 pg.init()
 
 
-size = (1920,1080)
+size = (1280,720)
 sky = (135,206,235)
 black = (1,1,1)
 grey = (120,120,120)
@@ -12,12 +12,24 @@ screen = pg.display.set_mode(size)
 width = screen.get_width()
 height = screen.get_height()
 
+time = pg.time.Clock()
 
+bg_image = pg.image.load("Call Of Fighters\img\screen pratique.jpg").convert_alpha()
+
+def background():
+    scaled_bg = pg.transform.scale(bg_image,size)
+    screen.blit(scaled_bg,(0,0))
+
+player1=Players.New_Players()
 
 game_on = True
 
-time = pg.time.Clock()
+
 while game_on:
+    player1.Movement(screen,height,width)
+    background()
+    player1.Draw(screen)
+
     for event in pg.event.get(): 
         if event.type == pg.QUIT: 
               game_on = False
@@ -25,8 +37,7 @@ while game_on:
                 if event.key==pg.K_ESCAPE:
                      game_on=False 
     mouse = pg.mouse.get_pos()
-
     pg.display.update()
-    screen.fill(sky)
     time.tick(60)
+
 pg.quit()
